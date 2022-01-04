a7fa483cccfbe05a3891d8fb4be9b63b4b54c0b9
3braheem <ibraahimsiri@gmail.com>
2022-01-04 10:31:41 -0800

    Test refactoring

--- src/test/Safemultisig.t.sol
+++ src/test/Safemultisig.t.sol
@@ -4,35 +4,46 @@ pragma solidity ^0.8.6;
 import "ds-test/test.sol";
 import "./utils/SafemultisigTest.sol";
 
-contract Submit is SafemultisigTest {
-
+contract Owners is SafemultisigTest {
     function testOwners() public {
         assertEq(sOwners.length, 1);
     }
-    
+}
+
+contract addOwner is SafemultisigTest {
     function testAddOwner() public {
 
     }
+}
 
+contract subOwner is SafemultisigTest {
     function testRemoveOwner() public {
 
     }
+}
 
+contract Submit is SafemultisigTest {
     function testSubmitTx() public {
         alice.submitTx(address(bob), 1 ether, "");
         // Transaction aliceTx = transactions[0];
-    }
+    }  
+}
 
+contract Confirm is SafemultisigTest {
     function testConfirmTx() public {
 
     }
+}
 
+contract Revoke is SafemultisigTest {
     function testRevokeConfirmation() public {
 
     }
+}
 
+contract Execute is SafemultisigTest {
     function testExecuteTx() public {
-        
+
     }
 }
 
